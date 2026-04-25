b4d7dee5f873295c6ebc6c4b53fbc5d0ac21483a
Mykhailo <misha16motto@gmail.com>
2026-04-25 11:28:06 +0300

    pushing 2

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ print("Hello, here you can play some music")
 auth = str,input("Which author do you want to hear?")
 random = str, input("Allright, what about to randomise it? ")
 random_choise(random)
-print(type(random))
-print(type(True))
+
 
 
 
