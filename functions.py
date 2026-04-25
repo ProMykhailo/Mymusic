a6c322314925d45c9df3b9c2513c4729afa849c1
Mykhailo <misha16motto@gmail.com>
2026-04-25 11:15:50 +0300

    music A-HA + begining

---
+++ functions.py
@@ -0,0 +1,5 @@
+def random_choise(choise):
+    if choise == "ok" or "True":
+        choise = True
+    else:
+        choise = False
--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hi")
+from functions import *
+
+print("Hello, here you can play some music")
+auth = str,input("Which author do you want to hear?")
+random = str, input("Allright, what about to randomise it? ")
+random_choise(random)
+print(type(random))
+print(type(True))
+
+
+
+
+
